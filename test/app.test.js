1f12f3182620a6beeb3d91b727513ed5fc6b77f3
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-07-19 16:36:25 -0700

    made tweet schema, creates a tweet with post, and pass test

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Tweet = require('../lib/models/Tweet');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +17,22 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('post a new tweet', () => {
+    return request(app)
+      .post('/api/v1/tweets')
+      .send({ 
+        handle: '@fake',
+        text: 'this is only a test'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          handle: '@fake',
+          text: 'this is only a test',
+          __v: 0
+        });
+      });
   });
 });
